5e5206cbc3755162a7c5f196db0938f85d0a5488
Gwen <akamitsunami@gmail.com>
2022-09-06 08:40:19 -0500

    update header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useEffect } from "react";
 import DarkToggle from "../components/DarkToggle";
 
@@ -23,17 +24,28 @@ export default function Header() {
 
     return (
         <header className="backdrop-blur-md bg-white dark:bg-gray-900 text-white py-4 fixed top-0 left-0 right-0">
-            <div className="container mx-auto flex flex-row justify-between items-center h-12 px-4">
-                <h1 className="text-2xl flex-grow text-black dark:text-white">
-                    TetraTsunami
-                </h1>
+            <div className="container mx-auto flex flex-row justify-between items-center h-12 px-4 text-black dark:text-white">
+                <Link href="/" passHref>
+                    <a className="hover:underline text-2xl flex-grow">
+                        TetraTsunami
+                    </a>
+                </Link>
                 <nav>
                     <ul className="flex flex-row items-center justify-around">
-                        <li className="mx-4 text-black dark:text-white">
-                            <a href="#about">About</a>
+                        <li className="mx-4">
+                            <Link href="/#about" passHref>
+                                <a className="hover:underline">About</a>
+                            </Link>
                         </li>
-                        <li className="mx-4 text-black dark:text-white">
-                            <a href="#projects">Projects</a>
+                        <li className="mx-4">
+                            <Link href="/#projects" passHref>
+                                <a className="hover:underline">Projects</a>
+                            </Link>
+                        </li>
+                        <li className="mx-4">
+                            <Link href="/media" passHref>
+                                <a className="hover:underline">Media</a>
+                            </Link>
                         </li>
                     </ul>
                 </nav>
