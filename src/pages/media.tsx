40e1b32c0954b534ba968630a4c1f5975822e874
Gwen <akamitsunami@gmail.com>
2022-09-27 20:51:33 -0500

    Improve media

--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -1,11 +1,45 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import NowPlayingDetail from "../components/NowPlayingDetail";
+import textStyles from "../styles/text.module.scss";
 
 const Media: NextPage = () => {
     return (
-        <section className="flex flex-col items-center justify-center h-screen p-8 container mx-auto">
-            <NowPlayingDetail />
-        </section>
+        <>
+            <Head>
+                <title>Media - Tsuni!</title>
+            </Head>
+            <main className="min-h-screen dark:text-white">
+                <section
+                    className="px-8 pt-32 pb-16
+                container mx-auto"
+                >
+                    <h2
+                        className={`text-6xl text-center font-bold ${textStyles.gradientTitleText}`}
+                    >
+                        <span className={textStyles["animateFadeUp-1"]}>
+                            Here&apos;s
+                        </span>{" "}
+                        <span className={textStyles["animateFadeUp-2"]}>
+                            what
+                        </span>{" "}
+                        <span className={textStyles["animateFadeUp-3"]}>
+                            I&apos;m
+                        </span>{" "}
+                        <span className={`h-16 ${textStyles["animateFadeUp-4"]}`}>
+                            listening
+                        </span>{" "}
+                        <span className={textStyles["animateFadeUp-5"]}>
+                            to.
+                        </span>
+                    </h2>
+                </section>
+                <section className="flex flex-col items-center justify-center p-8 container mx-auto">
+                    <h2 className="text-4xl mb-8">Now Playing</h2>
+                    <NowPlayingDetail />
+                </section>
+            </main>
+        </>
     );
 };
 
