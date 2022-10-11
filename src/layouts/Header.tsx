d89d8641d032925131aff15d0e726c3deaad6816
Gwen <akamitsunami@gmail.com>
2022-10-10 21:43:46 -0500

    look better when squished

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -42,7 +42,7 @@ export default function Header() {
 
     return (
         <header className="backdrop-blur-md bg-white dark:bg-gray-900 text-white p-3 fixed top-0 left-0 right-0">
-            <div className="container mx-auto flex flex-row justify-between items-center h-12 px-4 text-black dark:text-white">
+            <div className="container mx-auto flex flex-row flex-wrap sm:flex-nowrap justify-between items-center pt-4 sm:h-12 sm:pt-0 px-4 text-black dark:text-white">
                 <Link href="/" passHref>
                     <a className="hover:underline px-4 text-2xl">
                         TetraTsunami
@@ -51,10 +51,10 @@ export default function Header() {
                 <div className="flex-grow" />
                 <nav>
                     <ul className="flex flex-row items-center justify-around">
-                        <li className="mx-4">
+                        <li className="m-5">
                             <PageLink name="Home" href="/" />
                         </li>
-                        <li className="mx-4">
+                        <li className="m-5">
                             <PageLink name="Media" href="/media" />
                         </li>
                     </ul>
