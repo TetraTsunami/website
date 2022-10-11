086cc23214d7e35bad2c86a04024f4787f7b96c5
Gwen <akamitsunami@gmail.com>
2022-10-10 21:44:18 -0500

    make styles uniform, hint at content

--- src/components/cards/CertCard.tsx
+++ src/components/cards/CertCard.tsx
@@ -15,7 +15,7 @@ export default function CertCard({
     imageUrl: string | StaticImageData;
 }) {
     return (
-        <div className="relative group hover:z-10 border-4 border-grey-300 rounded-xl w-full hover:outline outline-4 outline-violet-600 transition-colors shadow-xl">
+        <div className="relative group hover:z-10 border-4 border-grey-300 rounded-xl w-full hover:outline outline-4 outline-violet-600 shadow-xl transition-transform duration-200 hover:scale-105">
             <a href={certUrl}>
                 <Image
                     src={imageUrl}
@@ -29,11 +29,11 @@ export default function CertCard({
             {collectionUrl && (
                 <a
                     href={collectionUrl}
-                    className="absolute duration-200 bottom-3 right-3 rounded-full shadow-md group-hover:scale-110"
+                    className="absolute bottom-3 right-3 rounded-full shadow-xl"
                 >
                     <FontAwesomeIcon
                         icon={faCodepen}
-                        className="p-4 bg-violet-300 text-black border-4 rounded-full hover:outline outline-4 outline-violet-400 hover:bg-violet-400"
+                        className="p-4 bg-violet-300 text-black border-4 rounded-full hover:outline outline-4 duration-200 transition-transform group-hover:scale-105 group-hover:hover:scale-110 outline-violet-600"
                         size={"xl" as SizeProp}
                     />
                     <p className="visible-hidden">Codepen collection</p>
--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -10,7 +10,7 @@ type ProjectCardProps = {
 
 export default function ProjectCard({ name, href, imageUrl, description, year }: ProjectCardProps) {
     return (
-        <section className="group border-4 border-grey-300 shadow-lg rounded-xl duration-200 overflow-hidden relative hover:outline outline-4 outline-violet-600 w-full h-52">
+        <section className="group border-4 border-grey-300 shadow-lg rounded-xl overflow-hidden relative hover:outline outline-4 outline-violet-600 w-full h-52 transition-transform duration-200 hover:scale-105">
             <a href={href}>
                 <Image
                     src={imageUrl}
@@ -19,14 +19,18 @@ export default function ProjectCard({ name, href, imageUrl, description, year }:
                     objectFit="cover"
                     className="bg-pink-200 dark:bg-gray-900"
                 />
-                <div className="p-6 min-h-full flex flex-col justify-center bg-white/90 dark:bg-black/90 shadow-lg relative top-full -translate-y-5 group-hover:-translate-y-full rounded-lg will-change-transform motion-safe:duration-500">
+                <div
+                    className="py-6 h-full w-4/5 mx-auto group-hover:w-full group-hover:px-6 flex flex-col justify-center 
+                bg-white/50 group-hover:bg-white/90 dark:bg-black/50 dark:group-hover:bg-black/90
+                shadow-lg relative top-full -translate-y-5 group-hover:-translate-y-full rounded-lg will-change-transform motion-safe:duration-500"
+                >
                     <span className="absolute top-0 right-5 italic text-sm font-semibold align-top text-left text-gray-900 dark:text-white">
                         {year}
                     </span>
                     <h3 className="text-lg text-gray-700 dark:text-white pt-6">
                         {name}
                     </h3>
-                    <p className="text-sm text-gray-600 dark:text-slate-300  pt-2 pb-4">
+                    <p className="text-sm text-gray-600 dark:text-slate-300 py-2">
                         {description}
                     </p>
                 </div>
