30e84c7dff76252f8f056218c2f18ec915e51fc0
Gwen <akamitsunami@gmail.com>
2022-12-23 21:58:30 -0600

    group plays in same album/parent

--- src/components/HistoryGrid.tsx
+++ src/components/HistoryGrid.tsx
@@ -19,37 +19,88 @@ export default function NowPlayingText() {
     
     if (error) return <Error className="w-96" />;
     if (!data) return <div className="w-96 h-screen" />;
+    let totalNonDuplicate = 0;
+    let duplicateAlbumCount = 0;
     return (
         <div className="grid gap-6 pt-3 mt-3 text-center grid-cols-1 sm:grid-cols-2 w-full max-w-4xl content-evenly">
             {history.data.map((item: any, index: number) => {
-                if (index >= 20) return;
-                return (<div
-                    key={index}
-                    className="dark:bg-gray-900/75 bg-gray-200/50 rounded-lg border-t-white/20 border-t-2 flex animate-fade-up"
-                    style={{ animationFillMode: "backwards", animationDelay: `${(index - index % 2) * 0.05}s` }}
-                >
-                    <picture className="float-left m-2 flex-shrink-0">
-                        <img
-                            src={item.imageUrl}
-                            alt={item.title}
-                            width={80}
-                            height={80}
-                            className="rounded-lg aspect-square object-cover bg-violet-300"
-                        />
-                    </picture>
-                    <div className="flex flex-col my-2 h-20 text-left flex-shrink whitespace-nowrap overflow-hidden">
-                        <h1 className="text-md overflow-hidden overflow-ellipsis">
-                            {item.title}
-                        </h1>
-                        <p className="text-sm text-gray-600 dark:text-gray-400 overflow-hidden overflow-ellipsis">
-                            {item.artist}
-                        </p>
-                        <p className="text-sm text-gray-500 w-full  overflow-hidden overflow-ellipsis">
-                            {item.parent}
-                        </p>
-                    </div>
-                </div>
-            )})}
+                if (totalNonDuplicate >= 20) return;
+                else if (history.data[index + 1]?.parent === item.parent) {
+                    duplicateAlbumCount++;
+                    return;
+                } else if (history.data[index - 1]?.parent === item.parent) {
+                    totalNonDuplicate++;
+                    duplicateAlbumCount++;
+                    return (
+                        <div
+                            key={item.title}
+                            className="dark:bg-gray-900/75 bg-gray-200/50 rounded-lg border-t-white/20 border-t-2 flex animate-fade-up animation-fill-backward"
+                            style={{
+                                animationDelay: `${
+                                    (index - (index % 2)) * 0.05
+                                }s`,
+                            }}
+                        >
+                            <picture className="float-left m-2 flex-shrink-0">
+                                <img
+                                    src={item.imageUrl}
+                                    alt={item.title}
+                                    width={80}
+                                    height={80}
+                                    className="rounded-lg aspect-square object-cover bg-violet-300"
+                                />
+                            </picture>
+                            <div className="flex flex-col my-2 h-20 text-left flex-shrink whitespace-nowrap overflow-hidden">
+                                <h1 className="text-md overflow-hidden overflow-ellipsis">
+                                    {item.parent}
+                                </h1>
+                                <p className="text-sm text-gray-600 dark:text-gray-400 overflow-hidden overflow-ellipsis">
+                                    {item.artist}
+                                </p>
+                            </div>
+                            <div className="flex-grow" />
+                            <div className="justify-self-end h-7 w-7 m-2 rounded-lg bg-violet-400 flex justify-center items-center">
+                                <p>{duplicateAlbumCount}</p>
+                            </div>
+                        </div>
+                    );
+                } else {
+                    totalNonDuplicate++;
+                    duplicateAlbumCount = 0;
+                    return (
+                        <div
+                            key={item.title}
+                            className="dark:bg-gray-900/75 bg-gray-200/50 rounded-lg border-t-white/20 border-t-2 flex animate-fade-up animation-fill-backward"
+                            style={{
+                                animationDelay: `${
+                                    (index - (index % 2)) * 0.05
+                                }s`,
+                            }}
+                        >
+                            <picture className="float-left m-2 flex-shrink-0">
+                                <img
+                                    src={item.imageUrl}
+                                    alt={item.title}
+                                    width={80}
+                                    height={80}
+                                    className="rounded-lg aspect-square object-cover bg-violet-300"
+                                />
+                            </picture>
+                            <div className="flex flex-col my-2 h-20 text-left flex-shrink whitespace-nowrap overflow-hidden">
+                                <h1 className="text-md overflow-hidden overflow-ellipsis">
+                                    {item.title}
+                                </h1>
+                                <p className="text-sm text-gray-600 dark:text-gray-400 overflow-hidden overflow-ellipsis">
+                                    {item.artist}
+                                </p>
+                                <p className="text-sm text-gray-500 w-full  overflow-hidden overflow-ellipsis">
+                                    {item.parent}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                }
+            })}
         </div>
     );
 }
