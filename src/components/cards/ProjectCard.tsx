e5e00328924471d20f0221fc0fa5b4022d2f589e
Tetra Fi <78718829+TetraTsunami@users.noreply.github.com>
2023-07-08 08:12:19 -0500

    Fix text wrapping differently on hover

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -8,7 +8,13 @@ type ProjectCardProps = {
     year: string;
 };
 
-export default function ProjectCard({ name, href, imageUrl, description, year }: ProjectCardProps) {
+export default function ProjectCard({
+    name,
+    href,
+    imageUrl,
+    description,
+    year,
+}: ProjectCardProps) {
     return (
         <section className="group border-4 border-grey-300 shadow-lg rounded-xl overflow-hidden relative hover:outline outline-4 outline-violet-600 w-full h-52 transition-transform duration-200 hover:scale-105">
             <a href={href}>
@@ -20,7 +26,7 @@ export default function ProjectCard({ name, href, imageUrl, description, year }:
                     className="bg-pink-200 dark:bg-gray-900 group-hover:scale-105 duration-1000"
                 />
                 <div
-                    className="py-6 h-full mx-auto group-hover:w-full group-hover:px-6 flex flex-col justify-center 
+                    className="py-6 h-full mx-auto w-full px-6 flex flex-col justify-center 
                 bg-white/75 group-hover:bg-white/90 dark:bg-black/75 dark:group-hover:bg-black/90
                 shadow-lg relative top-full -translate-y-5 group-hover:-translate-y-full rounded-lg will-change-transform motion-safe:duration-500"
                 >
@@ -37,5 +43,4 @@ export default function ProjectCard({ name, href, imageUrl, description, year }:
             </a>
         </section>
     );
-
 }
