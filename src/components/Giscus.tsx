6294ab8a6c67b90fc1a0e19267e8bac53238520f
TetraTsunami <78718829+TetraTsunami@users.noreply.github.com>
2024-11-06 00:12:57 -0600

    [fix] Add Giscus category and categoryId props

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -8,6 +8,8 @@ const Giscus = () => {
         id="comments"
         repo={process.env.NEXT_PUBLIC_GISCUS_REPO as any}
         repoId={process.env.NEXT_PUBLIC_GISCUS_REPO_ID as any}
+        category={process.env.NEXT_PUBLIC_GISCUS_CATEGORY as any}
+        categoryId={process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID as any}
         mapping="pathname"
         strict="1"
         reactionsEnabled="1"
