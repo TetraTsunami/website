2f4fbf393606738e954a984f5dcd257a18ab4b9c
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-04-13 15:51:42 -0500

    tailwind 4.0 changes

--- src/components/IconButtonRow1.tsx
+++ src/components/IconButtonRow1.tsx
@@ -1,55 +1,45 @@
-"use client"
+"use client";
 import { IconDefinition, SizeProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Tippy, { useSingleton } from "@tippyjs/react";
-import "tippy.js/dist/tippy.css";
 import textStyles from "../styles/text.module.scss";
 
 interface IconButtonRowProps {
-    icon: IconDefinition;
-    rel?: string;
-    href: string;
-    title: string;
+  icon: IconDefinition;
+  rel?: string;
+  href: string;
+  title: string;
 }
 
 export default function IconButtonRowLg({
-    buttons,
+  buttons,
 }: {
-    buttons: IconButtonRowProps[];
+  buttons: IconButtonRowProps[];
 }) {
-    const [source, target] = useSingleton();
-    // Buttons is an array of objects with the following properties:
-    // {
-    //     icon: FontAwesomeIcon,
-    //     href: string,
-    //     name: string,
-    // }
-    return (
-        <ul className="mx-4 my-2 flex flex-row items-center justify-between">
-            <Tippy
-                singleton={source}
-                theme="material"
-                moveTransition="transform 0.1s cubic-bezier(.17,.67,.83,.67)"
-            />
-            {buttons.map((button: IconButtonRowProps, index: number) => (
-                <li key={index} className="relative text-center">
-                    <Tippy content={button.title} singleton={target}>
-                        <a
-                            rel={button.rel}
-                            href={button.href}
-                            className={`px-3 py-2 ${textStyles["pastelTextHover"]
-                                }`}
-                            style={{ "--data-degree": index * 360/buttons.length} as React.CSSProperties}
-                        >
-                            <FontAwesomeIcon
-                                icon={button.icon}
-                                size={"xl" as SizeProp}
-                            />
-                            <p className="visible-hidden">{button.title}</p>
-                        </a>
-                    </Tippy>
-                </li>
-            ))}
-        </ul>
-    );
+  // Buttons is an array of objects with the following properties:
+  // {
+  //     icon: FontAwesomeIcon,
+  //     href: string,
+  //     name: string,
+  // }
+  return (
+    <ul className="mx-4 my-2 flex flex-row items-center justify-between">
+      {buttons.map((button: IconButtonRowProps, index: number) => (
+        <li key={index} className="relative text-center">
+          <a
+            rel={button.rel}
+            href={button.href}
+            className={`px-3 py-2 ${textStyles["pastelTextHover"]}`}
+            style={
+              {
+                "--data-degree": (index * 360) / buttons.length,
+              } as React.CSSProperties
+            }
+          >
+            <FontAwesomeIcon icon={button.icon} size={"xl" as SizeProp} />
+            <p className="visible-hidden">{button.title}</p>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/components/IconButtonRow2.tsx
+++ src/components/IconButtonRow2.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { IconDefinition, SizeProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Tippy, { useSingleton } from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 import textStyles from "../styles/text.module.scss";
 
@@ -20,7 +19,6 @@ export default function IconButtonRowLg({
   animate: boolean;
   animateDelay: number;
 }) {
-  const [source, target] = useSingleton();
   // Buttons is an array of objects with the following properties:
   // {
   //     icon: FontAwesomeIcon,
@@ -29,24 +27,16 @@ export default function IconButtonRowLg({
   // }
   return (
     <ul className="m-2 flex flex-row items-center justify-between">
-      <Tippy
-        singleton={source}
-        theme="material"
-        placement="bottom"
-        moveTransition="transform 0.1s cubic-bezier(.17,.67,.83,.67)"
-      />
       {buttons.map((button: IconButtonRowProps, index: number) => (
         <li key={index} className="relative text-center">
-          <Tippy content={button.title} singleton={target}>
-            <a
-              href={button.href}
-              className={`px-3 py-2 ${animate ? textStyles["animateFadeUp"] : "motion-safe:opacity-0"}`}
-              style={fadeUpDelay(index + animateDelay)}
-            >
-              <FontAwesomeIcon icon={button.icon} size={"xl" as SizeProp} />
-              <p className="visible-hidden">{button.title}</p>
-            </a>
-          </Tippy>
+          <a
+            href={button.href}
+            className={`px-3 py-2 ${animate ? textStyles["animateFadeUp"] : "motion-safe:opacity-0"}`}
+            style={fadeUpDelay(index + animateDelay)}
+          >
+            <FontAwesomeIcon icon={button.icon} size={"xl" as SizeProp} />
+            <p className="visible-hidden">{button.title}</p>
+          </a>
         </li>
       ))}
     </ul>
