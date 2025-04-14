01f7b217db806884a946570c8b9600b856e28dbc
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-04-14 15:49:10 -0500

    [fix] no more tippy

--- src/components/IconButtonRow2.tsx
+++ src/components/IconButtonRow2.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { IconDefinition, SizeProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "tippy.js/dist/tippy.css";
 import textStyles from "../styles/text.module.css";
 
 interface IconButtonRowProps {
